8eaf6a0af9d29c08b2d713dae606b2089ca7514c
DharaniSenthil-commits <dharanieastsenthilkumar@gmail.com>
2024-02-05 21:40:09 +0530

    upto properties

--- PythonCodewithMosh/container.py
+++ PythonCodewithMosh/container.py
@@ -1,21 +1,22 @@
 class TagCloud:
     def __init__(self) -> None:
-        self.tags = {}
+        # __tag --> mentioning it is private members
+        self.__tags = {}
 
     def add(self, tag):
-        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0)+1
+        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0)+1
 
     def __getitem__(self, tag):
-        return self.tags.get(tag, 0)
+        return self.__tags.get(tag, 0)
 
     def __setitem__(self, tag, count):
-        self.tags[tag] = count
+        self.__tags[tag] = count
 
     def __len__(Self):
-        return len(Self.tags)
+        return len(Self.__tags)
 
     def __iter__(self):
-        return iter(self.tags.items())
+        return iter(self.__tags.items())
 
 
 cloud = TagCloud()
@@ -24,6 +25,10 @@ cloud["python"] = 10
 print(len(cloud))
 cloud.add("python")
 cloud.add("Python")
-print(cloud.tags)
+
 for tag, count in cloud:
     print(tag, count)
+
+# print(cloud.tags["Python"]) --> will shows error
+print(cloud.__dict__)
+print(cloud._TagCloud__tags['python'])
